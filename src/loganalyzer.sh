1a98540ac1e3b1a3313a9bc8e94e743e34f44256
Jeffrin Goldwin <jeffcrjj@gmail.com>
2026-09-24 00:48:56 +0530

    dev: added top 10 paths, 404 paths and ips

--- src/loganalyzer.sh
+++ src/loganalyzer.sh
@@ -11,3 +11,17 @@ echo "Total number of logs: $( wc -l < "$1" )"
 echo "Uniqe IPs: $( awk '{ print $1 }' "$1" | sort -u | wc -l )"
 echo "Number of 2xx: $( awk ' $9 ~ /^2[0-9][0-9]$/ {print $9} ' "$1" | wc -l)"
 echo "Number of 4xx: $( awk ' $9 ~ /^4[0-9][0-9]$/ {print $9} ' "$1" | wc -l)"
+
+echo "==========================================================="
+echo
+
+echo "Top 10 Ips:"
+awk '{ print $1 }' "$1" | sort | uniq -c | sort -r | head
+echo
+
+echo "Top 10 Paths:"
+awk '{ print $7 }' "$1" | sort | uniq -c | sort -r | head
+echo
+
+echo "Top 10 404 Paths:"
+awk ' $9==404 { print $7 }' "$1" | sort | uniq -c | sort -r | head
